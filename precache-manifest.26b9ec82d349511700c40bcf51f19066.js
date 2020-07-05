08e1bc738b4608e22a42a466208272eb90500590
David <david.popescu10@gmail.com>
2020-07-05 15:12:22 +0300

    Updates

---
+++ precache-manifest.26b9ec82d349511700c40bcf51f19066.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c1289d6dd347fdaf6c190f2c900a26f",
+    "url": "/Cartzilla/index.html"
+  },
+  {
+    "revision": "b0e295e71d99b237dcee",
+    "url": "/Cartzilla/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "89a2a9983d116256aa74",
+    "url": "/Cartzilla/static/css/main.0781072b.chunk.css"
+  },
+  {
+    "revision": "b0e295e71d99b237dcee",
+    "url": "/Cartzilla/static/js/2.0a60ec06.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Cartzilla/static/js/2.0a60ec06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89a2a9983d116256aa74",
+    "url": "/Cartzilla/static/js/main.0d59e0e7.chunk.js"
+  },
+  {
+    "revision": "c5c2e58a957fb066a9ef",
+    "url": "/Cartzilla/static/js/runtime-main.d5846a4f.js"
+  },
+  {
+    "revision": "b3d0902b533ff4c4f1698a2f96ddabab",
+    "url": "/Cartzilla/static/media/Rubik-Regular.b3d0902b.ttf"
+  }
+]);
